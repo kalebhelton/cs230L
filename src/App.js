8fc28542639d175f2adabe55b9cd2e7544e3870f
kalebhelton <kah00028@mix.wvu.edu>
2024-03-02 16:52:41 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import navBar from './Navbar.js';
 function App() {
   return (
     <div className="App">
-        <navBar />
         <h1>CS 230L</h1>
         <h2>Section 01</h2>
         <p>WVU ID: 800393145</p>
         <p>Hi I am Kaleb</p>
 
         <Card />
+        <navBar />
 
     </div>
   );
