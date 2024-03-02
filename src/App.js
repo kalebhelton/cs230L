63c8e4e4e7c1bb6341717cdeace73a779a4b07d6
kalebhelton <kah00028@mix.wvu.edu>
2024-03-02 17:10:18 -0500

    Updated App.js + Card.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
         <p>Hi I am Kaleb</p>
 
         <Card />
-        <navBar />
 
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,20 @@
 export default function Card(){
     return(
-        <div class="card bg-primary text-white text-center m-2" style="width: 300px;">
-            <div class = "card-header text-center">Card 01</div>
+        <><div class="card text-white bg-primary text-center m-2" style="width: 300px;">
+            <div class="card-header text-center ">Card 01</div>
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on teh card title + make up the bulk of teh card's content.</p>
+            </div>
+        </div><div class="card text-white bg-danger text-center m-2" style="width: 300px;">
+                <div class="card-header text-center ">Card 02</div>
                 <div class="card-body">
                     <p class="card-text">Some quick example text to build on teh card title + make up the bulk of teh card's content.</p>
                 </div>
-        </div>
+            </div><div class="card text-white bg-success text-center m-2" style="width: 300px;">
+                <div class="card-header text-center ">Card 03</div>
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on teh card title + make up the bulk of teh card's content.</p>
+                </div>
+            </div></>
     );
 }
